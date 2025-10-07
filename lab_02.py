5727354e0cdf9cb82e373645c33dec42719d8b69
Tyler Overbeek <tylerov@umich.edu>
2025-10-06 22:32:30 -0400

    Pls be last commit

--- lab_02.py
+++ lab_02.py
@@ -18,13 +18,16 @@ Here they are;
 --------------------------------------------------------
 TO REPRODUCE THE VALUES AND PLOTS IN MY REPORT, DO THIS:
 
-1. To verify my code works for questions 1 and 2, and to replicate the graph in Dan's lab report, run 
-    'verification_plot()'
-2. To get phase diagrams for question 3, run
-    'phase_diagram()'
+Firstly, run my code, then
+1. To reproduce figure 1, and to replicate the graph in Dan's lab report, run 'verification_plot()'
+2. To reproduce figure 2 in my report, run 'error_plots_comp_euler()'
+3. To reproduce figure 3 in my report, run 'error_plots_comp_rk8()'
+4. To reproduce figure 4 in my report, run 'error_plots_pred_prey_euler()'
+5. To reproduce figure 5 in my report, run 'error_plots_pred_prey_rk8()'
+6. To reproduce figure 6 in my report, run 'varying_initial_conditions_comp()'
+7. To reproduce figure 7 in my report, run 'varying_initial_conditions_pred_prey()'
+8. To reproduce figure 8 in my report, run 'phase_diagram()'
 
-This should be all. If you'd like to change any default values, you're more than welcome to by changing the values
-each of the 'euler_solve' and 'solve_rk8" default values in my 'verification_plot' or 'phase_diagram' functions. 
 '''
 
 import numpy as np
@@ -173,6 +176,10 @@ def solve_rk8(func, N1_init = 0.5, N2_init = 0.5, dT = 10, t_final = 100.0,
     return N1, N2, time
 
 def verification_plot():
+    '''
+    This functions simply returns a plot verifying my data with the plot in
+    Dan's lab report.
+    '''
     fig, axes = plt.subplots(1,2, figsize=[12.5, 10])
     N1, N2, time = euler_solve(func = dNdt_comp, N1_init = 0.3, 
                                N2_init = 0.6, dT = 1)
@@ -203,14 +210,239 @@ def verification_plot():
     fig.tight_layout()
     plt.show()
 
+def error_plots_comp_euler():
+    '''
+    This functions simply returns a plot of errors using Euler's method,
+    for the competition model
+    '''
+    fig, ax = plt.subplots(1,1, figsize=[12.5, 10])
+    N1_init, N2_init, time = euler_solve(func = dNdt_comp, N1_init = 0.3, 
+                                         N2_init = 0.6, dT = 1)
+    ax.plot(time, N1_init, label = 'N1_init')
+    ax.plot(time, N2_init, label = 'N2_init')
+
+    N1_dthalf, N2_dthalf, time = euler_solve(func = dNdt_comp, N1_init = 0.3, 
+                                             N2_init = 0.6, dT = 0.5)
+    ax.plot(time, N1_dthalf, label = 'N1 DT Halved')
+    ax.plot(time, N2_dthalf, label = 'N2 DT Halved')
+
+    N1_double, N2_double, time = euler_solve(func = dNdt_comp, N1_init = 0.3, 
+                                         N2_init = 0.6, dT = 2)
+    ax.plot(time, N1_double, label = 'N1 DT Doubled')
+    ax.plot(time, N2_double, label = 'N2 DT Doubled')
+
+    N1_quadruple, N2_quadruple, time = euler_solve(func = dNdt_comp, N1_init = 0.3, 
+                                             N2_init = 0.6, dT = 4)
+    ax.plot(time, N1_quadruple, label = 'N1 DT Quadrupled')
+    ax.plot(time, N2_quadruple, label = 'N2 DT Quadrupled')
+    ax.legend()
+    ax.set_xlabel('Time (years)')
+    ax.set_ylabel('Population/Carrying Capacity')
+    ax.set_title('Lotka-Volterra Competition Model')
+    plt.show()
+
+def error_plots_comp_rk8():
+    '''
+    This functions simply returns a plot of errors using Range-Kutta's method, 
+    for the competition model
+    '''
+    fig, ax = plt.subplots(1,1, figsize=[12.5, 10])
+    N1_rk8_init, N2_rk8_init, time_rk8 = solve_rk8(func = dNdt_comp, N1_init = 0.3,
+                                                   N2_init = 0.6, dT = 1)
+    ax.plot(time_rk8, N1_rk8_init, label = 'N1_rk8_init')
+    ax.plot(time_rk8, N2_rk8_init, label = 'N2_rk8_init')
+
+    N1_rk8_dthalf, N2_rk8_dthalf, time_rk8 = solve_rk8(func = dNdt_comp, N1_init = 0.3,
+                                                       N2_init = 0.6, dT = 0.5)
+    ax.plot(time_rk8, N1_rk8_dthalf, label = 'N1_rk8 DT Halved')
+    ax.plot(time_rk8, N2_rk8_dthalf, label = 'N2_rk8 DT Halved')
+    N1_rk8_double, N2_rk8_double, time_rk8 = solve_rk8(func = dNdt_comp, N1_init = 0.3,
+                                                   N2_init = 0.6, dT = 2)
+    ax.plot(time_rk8, N1_rk8_double, '--', label = 'N1_rk8 DT Doubled', color = 'pink')
+    ax.plot(time_rk8, N2_rk8_double, label = 'N2_rk8 DT Doubled')
+
+    N1_rk8_quadruple, N2_rk8_quadruple, time_rk8 = solve_rk8(func = dNdt_comp, N1_init = 0.3,
+                                                       N2_init = 0.6, dT = 4)
+    ax.plot(time_rk8, N1_rk8_quadruple, '--', label = 'N1_rk8 DT Quadrupled')
+    ax.plot(time_rk8, N2_rk8_quadruple, label = 'N2_rk8 DT Quadrupled')
+    ax.legend()
+    ax.set_xlabel('Time (years)')
+    ax.set_ylabel('Population/Carrying Capacity')
+    ax.set_title('Lotka-Volterra Competition Model')
+    plt.show()
+
+def error_plots_pred_prey_euler():
+    '''
+    This functions simply returns a plot of errors using Euler's method, 
+    for the predator-prey models
+    '''
+    fig, ax = plt.subplots(1,1, figsize=[12.5, 10])
+    N1_init, N2_init, time = euler_solve(func = dNdt_pred_prey, N1_init = 0.3,
+                                         N2_init = 0.6, dT = 0.05)
+    ax.plot(time, N1_init, label = 'N1_init')
+    ax.plot(time, N2_init, label = 'N2_init')
+
+    N1_halved, N2_halved, time = euler_solve(func = dNdt_pred_prey, N1_init = 0.3,
+                                         N2_init = 0.6, dT = 0.025)
+    ax.plot(time, N1_halved, label = 'N1 DT Halved')
+    ax.plot(time, N2_halved, label = 'N2 DT Halved')
+
+    N1_double, N2_double, time = euler_solve(func = dNdt_pred_prey, N1_init = 0.3,
+                                         N2_init = 0.6, dT = 0.1)
+    ax.plot(time, N1_double, label = 'N1 DT Double')
+    ax.plot(time, N2_double, label = 'N2 DT Double')
+
+    N1_quad, N2_quad, time = euler_solve(func = dNdt_pred_prey, N1_init = 0.3,
+                                         N2_init = 0.6, dT = 0.2)
+    ax.plot(time, N1_quad, label = 'N1 DT Quadruple')
+    ax.plot(time, N2_quad, label = 'N2 DT Quadruple')
+    ax.legend()
+    ax.set_xlabel('Time (years)')
+    ax.set_ylabel('Population/Carrying Capacity')
+    ax.set_title('Lotka-Volterra Predator-Prey Model') 
+    fig.tight_layout()
+    plt.show()
+
+def error_plots_pred_prey_rk8():
+    '''
+    This functions simply returns a plot of errors using Range-Kutta's method, 
+    for the predator-prey models
+    '''
+    fig, ax = plt.subplots(1,1, figsize=[12.5, 10])
+    N1_rk8_init, N2_rk8_init, time_rk8 = solve_rk8(func = dNdt_pred_prey, 
+                                               N1_init = 0.3, N2_init = 0.6, 
+                                               dT = 0.05)
+    ax.plot(time_rk8, N1_rk8_init, label = 'N1_rk8_init')
+    ax.plot(time_rk8, N2_rk8_init, label = 'N2_rk8_init')
+
+    N1_rk8_halved, N2_rk8_halved, time_rk8 = solve_rk8(func = dNdt_pred_prey, 
+                                               N1_init = 0.3, N2_init = 0.6, 
+                                               dT = 0.025)
+    ax.plot(time_rk8, N1_rk8_halved, label = 'N1_rk8 Halved')
+    ax.plot(time_rk8, N2_rk8_halved, label = 'N2_rk8 Halved')
+
+    N1_rk8_double, N2_rk8_double, time_rk8 = solve_rk8(func = dNdt_pred_prey, 
+                                               N1_init = 0.3, N2_init = 0.6, 
+                                               dT = 0.1)
+    ax.plot(time_rk8, N1_rk8_double, label = 'N1_rk8 Double')
+    ax.plot(time_rk8, N2_rk8_double, label = 'N2_rk8 Double')
+
+    N1_rk8_quad, N2_rk8_quad, time_rk8 = solve_rk8(func = dNdt_pred_prey, 
+                                               N1_init = 0.3, N2_init = 0.6, 
+                                               dT = 0.2)
+    ax.plot(time_rk8, N1_rk8_quad, label = 'N1_rk8 Quadrupled')
+    ax.plot(time_rk8, N2_rk8_quad, label = 'N2_rk8 Quadrupled')
+    ax.legend()
+    ax.set_xlabel('Time (years)')
+    ax.set_ylabel('Population/Carrying Capacity')
+    ax.set_title('Lotka-Volterra Predator-Prey Model') 
+    fig.tight_layout()
+    plt.show()
+
+def varying_initial_conditions_comp():
+    '''
+    This functions simply returns a plot of different initial conditions
+    using Euler's method, for the competition models
+    '''
+    fig, axes = plt.subplots(1,2, figsize=[12.5, 10])
+    N1_init, N2_init, time = euler_solve(func = dNdt_comp, N1_init = 0.3, 
+                               N2_init = 0.6, dT = 1)
+    axes[0].plot(time, N1_init, label = 'N1_init')
+    axes[0].plot(time, N2_init, label = 'N2_init')
+    N1_half, N2_half, time = euler_solve(func = dNdt_comp, N1_init = 0.15, 
+                               N2_init = 0.3, dT = 1)
+    axes[0].plot(time, N1_half, label = 'N1 Pop. Halved')
+    axes[0].plot(time, N2_half, label = 'N2 Pop. Halved')
+    N1_2, N2_2, time = euler_solve(func = dNdt_comp, N1_init = 0.2, 
+                               N2_init = 0.4, dT = 1)
+    axes[0].plot(time, N1_2, label = 'N1 Pop. = 0.2')
+    axes[0].plot(time, N2_2, label = 'N2 Pop. = 0.4')
+    axes[0].legend()
+    axes[0].set_xlabel('Time (years)')
+    axes[0].set_ylabel('Population/Carrying Capacity')
+    axes[0].set_title('Lotka-Volterra Competition Model')
+
+    N1_rk8_init, N2_rk8_init, time_rk8 = solve_rk8(func = dNdt_comp, N1_init = 0.3,
+                                                   N2_init = 0.6, dT = 1)
+    axes[1].plot(time_rk8, N1_rk8_init, label = 'N1_rk8_init')
+    axes[1].plot(time_rk8, N2_rk8_init, label = 'N2_rk8_init')
+
+    N1_rk8_half, N2_rk8_half, time_rk8 = solve_rk8(func = dNdt_comp, N1_init = 0.15,
+                                                       N2_init = 0.3, dT = 1)
+    axes[1].plot(time_rk8, N1_rk8_half, label = 'N1_rk8 Pop. Halved')
+    axes[1].plot(time_rk8, N2_rk8_half, label = 'N2_rk8 Pop. Halved')
+    N1_rk8_2, N2_rk8_2, time_rk8 = solve_rk8(func = dNdt_comp, N1_init = 0.2,
+                                                   N2_init = 0.4, dT = 1)
+    axes[1].plot(time_rk8, N1_rk8_2, label = 'N1_rk8 Pop. = 0.2')
+    axes[1].plot(time_rk8, N2_rk8_2, label = 'N2_rk8 Pop. = 0.4')
+    axes[1].legend()
+    axes[1].set_xlabel('Time (years)')
+    axes[1].set_ylabel('Population/Carrying Capacity')
+    axes[1].set_title('Lotka-Volterra Competition Model') 
+    fig.tight_layout()
+    plt.show()
+
+def varying_initial_conditions_pred_prey():
+    '''
+    This functions simply returns a plot of different initial conditions
+    using Euler's method, for the predator-prey models
+    '''
+    fig, axes = plt.subplots(1,2, figsize=[12.5, 10])
+    N1_init, N2_init, time = euler_solve(func = dNdt_pred_prey, N1_init = 0.3, 
+                                         N2_init = 0.6, dT = 1)
+    axes[0].plot(time, N1_init, label = 'N1_init')
+    axes[0].plot(time, N2_init, label = 'N2_init')
+    N1_half, N2_half, time = euler_solve(func = dNdt_pred_prey, N1_init = 0.15, 
+                                         N2_init = 0.3, dT = 1)
+    axes[0].plot(time, N1_half, label = 'N1 Pop. Halved')
+    axes[0].plot(time, N2_half, label = 'N2 Pop. Halved')
+    N1_2, N2_2, time = euler_solve(func = dNdt_pred_prey, N1_init = 0.2, 
+                                   N2_init = 0.4, dT = 1)
+    axes[0].plot(time, N1_2, label = 'N1 Pop. = 0.2')
+    axes[0].plot(time, N2_2, label = 'N2 Pop. = 0.4')
+    axes[0].legend()
+    axes[0].set_xlabel('Time (years)')
+    axes[0].set_ylabel('Population/Carrying Capacity')
+    axes[0].set_title('Lotka-Volterra Predator_Prey Model')
+
+    N1_rk8_init, N2_rk8_init, time_rk8 = solve_rk8(func = dNdt_pred_prey, N1_init = 0.3,
+                                                   N2_init = 0.6, dT = 1)
+    axes[1].plot(time_rk8, N1_rk8_init, label = 'N1_rk8_init')
+    axes[1].plot(time_rk8, N2_rk8_init, label = 'N2_rk8_init')
+
+    N1_rk8_half, N2_rk8_half, time_rk8 = solve_rk8(func = dNdt_pred_prey, N1_init = 0.15,
+                                                   N2_init = 0.3, dT = 1)
+    axes[1].plot(time_rk8, N1_rk8_half, label = 'N1_rk8 Pop. Halved')
+    axes[1].plot(time_rk8, N2_rk8_half, label = 'N2_rk8 Pop. Halved')
+    N1_rk8_2, N2_rk8_2, time_rk8 = solve_rk8(func = dNdt_pred_prey, N1_init = 0.2,
+                                             N2_init = 0.4, dT = 1)
+    axes[1].plot(time_rk8, N1_rk8_2, label = 'N1_rk8 Pop. = 0.2')
+    axes[1].plot(time_rk8, N2_rk8_2, label = 'N2_rk8 Pop. = 0.4')
+    axes[1].legend()
+    axes[1].set_xlabel('Time (years)')
+    axes[1].set_ylabel('Population/Carrying Capacity')
+    axes[1].set_title('Lotka-Volterra Predator_Prey Model') 
+    fig.tight_layout()
+    plt.show()
+
 def phase_diagram():
+    '''
+    This functions simply returns a plot of different initial conditions
+    using Euler's method, for the competition models, as phase diagrams. 
+    '''
     fig, ax = plt.subplots(1, 1, figsize=[10,8])
     N1, N2, time = euler_solve(func = dNdt_pred_prey, N1_init = 0.3, N2_init = 0.6, 
-                         dT = 0.05)
+                               dT = 0.05)
     N1_rk8, N2_rk8, time_rk8 = solve_rk8(func = dNdt_pred_prey, N1_init = 0.3, 
                                          N2_init = 0.6, dT = 0.05)
-    ax.plot(N1, N2, label = 'Predator vs Prey')
-    ax.plot(N1_rk8, N2_rk8, '--', label = 'Predator vs Prey RK8')
+    ax.plot(N1, N2, label = 'Initial Conditions')
+    ax.plot(N1_rk8, N2_rk8, '--', label = 'Initial Conditions RK8')
+    N1_2, N2_2, time = euler_solve(func = dNdt_pred_prey, N1_init = 0.2, N2_init = 0.4, 
+                                   dT = 0.05)
+    ax.plot(N1_2, N2_2, label = 'N1 Pop. = 0.2, N2 Pop. = 0.4')
+    N1_1, N2_1, time = euler_solve(func = dNdt_pred_prey, N1_init = 0.35, N2_init = 0.7, 
+                                             dT = 0.05)
+    ax.plot(N1_1, N2_1, label = 'N1 Pop. = 0.35, N2 Pop. = 0.7')
     ax.legend()
     ax.set_xlabel('Prey Population')
     ax.set_ylabel('Predator Population')
