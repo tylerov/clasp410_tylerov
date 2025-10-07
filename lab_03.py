fd9a0d99fbf51e593defbb1431f8e31d39939aad
Tyler Overbeek <tylerov@umich.edu>
2025-10-07 11:19:59 -0400

    Commit from tuesday lecture

--- lab_03.py
+++ lab_03.py
@@ -28,9 +28,9 @@ def solve_heat(x_stop = 1., t_stop = 0.2, dx = 0.2, dt = 0.02, c2 = 1):
     N = int(t_stop / dt)
     M = int(x_stop / dx)
 
+    # Set up space and time grid
     t = np.linspace(0, t_stop, N)
     x = np.linspace(0, x_stop, M)
-    # Set up space and time grid
 
     # Create solution matrix; set initial conditions
     U = np.zeros([M, N])
@@ -41,7 +41,7 @@ def solve_heat(x_stop = 1., t_stop = 0.2, dx = 0.2, dt = 0.02, c2 = 1):
 
     # Solve our equation 
     for j in range(N-1):
-        U[1:N-1, j+1] = (1-2*r) * U[1:M-1, j] + r*(U[2:M, j] + U[:M-2, j])
+        U[1:M-1, j+1] = (1-2*r) * U[1:M-1, j] + r*(U[2:M, j] + U[:M-2, j])
 
     # Return our pretty solution to the caller
     return t, x, U
