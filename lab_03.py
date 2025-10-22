725a626f42e6638226755622df8b1ac864f4998e
Tyler Overbeek <tylerov@umich.edu>
2025-10-21 20:28:36 -0400

    Parts 1 and 2, figuring out 3

--- lab_03.py
+++ lab_03.py
@@ -35,8 +35,69 @@ def temp_kanger(t):
 
     return t_amp*np.sin(np.pi/180 * t - np.pi/2) + t_kanger.mean()
 
-def solve_heat(x_stop = 100, t_stop = 3650., dx = 1, dt = 1, c2 = 0.25,
+def solve_heat(x_stop = 1, t_stop = 0.2, dx = 0.2, dt = 0.02, c2 = 1,
                lowerbound = 0, upperbound = 0):
+    '''
+    A function for solving the heat equation
+
+    Parameters
+    ----------
+    FILL THIS OUT AND DO NOT FORGET
+
+    c2: Float
+        c^2, the square of the diffusion coeffiecient
+    Returns
+    -------
+    x, t: 1D Numpy arrays
+        Space and time values, respectively
+    U : Numpy array
+        The solution of the heat equation, size is nSpace * nTime
+    '''
+
+    # Check our stability criterion:
+    dt_max = dx**2 / (2*c2)
+    if dt > dt_max:
+       raise ValueError(f'DANGER: dt={dt} > dt_max = {dt_max}.')
+
+    # Get grid sizes (+ 1 to include "0" as well)
+    N = int(t_stop / dt) + 1
+    M = int(x_stop / dx) + 1
+
+    # Set up space and time grid
+    t = np.linspace(0, t_stop, N)
+    x = np.linspace(0, x_stop, M)
+
+    # Create solution matrix; set initial conditions
+    U = np.zeros([M, N])
+    U[:,0] = 4*x - 4*x**2
+
+    #Get our "r" coefficient
+    r = c2 * (dt/dx**2)
+
+    # Solve our equation 
+    for j in range(N-1):
+        U[1:M-1, j+1] = (1-2*r) * U[1:M-1, j] + r*(U[2:M, j] + U[:M-2, j])
+        # Apply Neumann BC's
+        if lowerbound is None:
+            U[0, j + 1] = U[1, j + 1]
+        elif callable(lowerbound):
+            U[0, j + 1] = lowerbound(t[j + 1])
+        else: 
+            U[0, j + 1] = lowerbound
+        if upperbound is None:
+            U[-1, j + 1] = U[-2, j + 1]
+        elif callable(upperbound):
+            U[-1, j + 1] = upperbound(t[j + 1])
+        else:
+            U[-1, j + 1] = upperbound
+    
+    # Print [U] to verify code
+    print([U])
+    # Return our pretty solution to the caller
+    return t, x, U
+
+def solve_heat_Kanger(x_stop = 100, t_stop = 3650, dx = 0.5, dt = 1, c2 = 0.25,
+                      lowerbound = temp_kanger, upperbound = 5):
     '''
     A function for solving the heat equation
 
@@ -70,9 +131,8 @@ def solve_heat(x_stop = 100, t_stop = 3650., dx = 1, dt = 1, c2 = 0.25,
     t = np.linspace(0, t_stop, N)
     x = np.linspace(0, x_stop, M)
 
-    # Create solution matrix; set initial conditions
+    # Create solution matrix
     U = np.zeros([M, N])
-    U[:,0] = 4*x - 4*x**2
 
     #Get our "r" coefficient
     r = c2_updated * (dt/dx**2)
@@ -94,6 +154,8 @@ def solve_heat(x_stop = 100, t_stop = 3650., dx = 1, dt = 1, c2 = 0.25,
         else:
             U[-1, j + 1] = upperbound
     
+    # Print [U] to verify code
+    print([U])
     # Return our pretty solution to the caller
     return t, x, U
 
@@ -130,9 +192,11 @@ def plot_heatsolve(t, x, U, dt = 1, title = None, **kwargs):
 
     # Add labels to stuff
     cbar.set_label(r'Temperature ($^{\circ}C$)')
-    ax.set_xlabel('Time ($s$)')
+    ax.set_xlabel('Time ($days$)')
     ax.set_ylabel('Depth ($m$)')
+    plt.gca().invert_yaxis()
     ax.set_title(title)
+
     # Set indexing for the final year of results:
     loc = int(-365/dt) # Final 365 days of the result.
 
@@ -143,9 +207,10 @@ def plot_heatsolve(t, x, U, dt = 1, title = None, **kwargs):
     #Create a temp profile plot:
     fig, ax2 = plt.subplots(1, 1, figsize=(10, 8))
     ax2.plot(winter, x, label='Winter')
-    ax2.plot(summer, x, label='Summer')
+    ax2.plot(summer, x, '--', label='Summer')
     ax2.set_xlabel(r'Temperature ($^{\circ}C$)')
     ax2.set_ylabel('Depth ($m$)')
+    plt.gca().invert_yaxis()
 
     fig.tight_layout()
     plt.show()
