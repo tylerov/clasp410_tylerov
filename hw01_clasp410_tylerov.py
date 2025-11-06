7402ed1763cf2f497c8fc346459e24d0f969b880
Tyler Overbeek <tylerov@umich.edu>
2025-11-06 11:11:34 -0500

    Commit

--- hw01_clasp410_tylerov.py
+++ hw01_clasp410_tylerov.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 '''
-Doc String
+Can solar forcing be accounted for climate change? In this code, 
+we will show measured temperature as their anomalies from 1900 to 
+2000, and then show expected temperatures given our T_E equation, 
+and then compare and make conclusions on our hypothesis. 
 '''
 import numpy as np
 import matplotlib.pyplot as plt
@@ -42,14 +45,19 @@ def verify_code():
     print('Numerical solution is: ', T_earth_real)
     print('Difference in solutions is: ', T_real - T_earth_real)
 
+# Years in an array
 year = np.array([1900, 1950, 2000])
-
+# Measured Solar forcing in an array
 s0 = np.array([1365, 1366.5, 1368])
+# Measured temperature anomaly in an array
 t_anom = np.array([-0.4, 0, 0.4])
 
+# Equation to find expected temperature given a solar forcing
 T_solar = solve_temp(0.33, s0)
+# Measured temperature given a solar forcing
 T_measured = T_solar[1] + t_anom
 
+# Plot that shows us trends in measured vs. expected Temperatures
 fig, ax = plt.subplots(1,1, figsize = (8,8))
 ax.plot(year, T_measured, label = 'Measured Temperature')
 ax.plot(year, T_solar, label = 'Solar forcing predicted temperature')
